9587d392859dee8bd6e5155b8546abe1afc8e662
Ishrath1 <127918732+Ishrath1@users.noreply.github.com>
2024-10-09 16:53:28 +0400

    v1 toujours...

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-#divers tests...
-
-import requests
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-page = requests.get('https://brightdata.com/blog/how-tos/web-scraping-with-python#need')
-print(page)
+import openpyxl
+
+excel = openpyxl.Workbook()
+sheet = excel.active
+sheet.title="Egalité"
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto('https://catalogue.univ-reunion.fr/primo-explore/search?query=any,contains,%C3%A9galit%C3%A9&tab=default_tab&search_scope=LSS_TOUT&vid=URN&offset=0')
+    contenu = page.content() 
+    soup = BeautifulSoup(contenu, 'lxml')
+
+
+
+    div = soup.find('div', {'class':'list-item-wrapper'})
+    print(div.prettify)
+    div_cont = div.find_all('prm-brief-result-container', {'class':'list-item'})
+    print(div_cont)
+
+
+
+
+
+
+
+
+    browser.close()
