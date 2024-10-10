8bd55974b40baba5f3426c885594112986b3dc69
Ishrath1 <127918732+Ishrath1@users.noreply.github.com>
2024-10-10 14:10:50 +0400

    v1....

--- main.py
+++ main.py
@@ -12,14 +12,7 @@ with sync_playwright() as p:
     page.goto('https://catalogue.univ-reunion.fr/primo-explore/search?query=any,contains,%C3%A9galit%C3%A9&tab=default_tab&search_scope=LSS_TOUT&vid=URN&offset=0')
     contenu = page.content() 
     soup = BeautifulSoup(contenu, 'lxml')
-
-
-
-    div = soup.find('div', {'class':'list-item-wrapper'})
-    print(div.prettify)
-    div_cont = div.find_all('prm-brief-result-container', {'class':'list-item'})
-    print(div_cont)
-
+    print(soup.prettify())
 
 
 
